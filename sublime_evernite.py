f94c931066d0830080b12a23cbbe0f0c465dcf53
jamiesun <jamiesun.net@gmail.com>
2012-07-15 13:57:36 +0800

    ...

--- sublime_evernite.py
+++ sublime_evernite.py
@@ -5,6 +5,7 @@ sys.path.append("lib")
 import hashlib
 import binascii
 import time
+from functools import wraps
 import thrift.protocol.TBinaryProtocol as TBinaryProtocol
 import thrift.transport.THttpClient as THttpClient
 import evernote.edam.userstore.UserStore as UserStore
@@ -13,7 +14,7 @@ import evernote.edam.notestore.NoteStore as NoteStore
 import evernote.edam.type.ttypes as Types
 import evernote.edam.error.ttypes as Errors
 
-# import sublime,sublime_plugin
+import sublime,sublime_plugin
 
 
 consumer_key = 'jamiesun-2467'
@@ -21,40 +22,62 @@ consumer_secret ='7794453e92251986'
 evernoteHost = "www.evernote.com"
 userStoreUri = "https://" + evernoteHost + "/edam/user"
 
-userStoreHttpClient = THttpClient.THttpClient(userStoreUri)
-userStoreProtocol = TBinaryProtocol.TBinaryProtocol(userStoreHttpClient)
-userStore = UserStore.Client(userStoreProtocol)
 
-# versionOK = userStore.checkVersion("Evernote EDAMTest (Python)",
-#                                    UserStoreConstants.EDAM_VERSION_MAJOR,
-#                                    UserStoreConstants.EDAM_VERSION_MINOR)
-# print "Is my Evernote API version up to date? ", str(versionOK)
 
-authresult = userStore.authenticate('jamiesun','wjt594123',consumer_key,consumer_secret)
 
-print authresult
+settings = sublime.load_settings("SublimeEvernote.sublime-settings")
 
-noteStoreUrl = authresult.noteStoreUrl
-authToken = authresult.authenticationToken
+def process_error(func):
+    @wraps(func)
+    def func_warp(*args,**kwargs):
+        try:
+            return func(*args,**kwargs)
+        except Exception, e:
+            sublime.error_message("error:%s"%e)  
+    return func_warp
 
-noteStoreHttpClient = THttpClient.THttpClient(noteStoreUrl)
-noteStoreProtocol = TBinaryProtocol.TBinaryProtocol(noteStoreHttpClient)
-noteStore = NoteStore.Client(noteStoreProtocol)
+class SendToEvernoteCommand(sublime_plugin.TextCommand):
+    def __init__(self,view):
+        self.view = view    
+        self.window = view.window()
 
-# List all of the notebooks in the user's account        
-notebooks = noteStore.listNotebooks(authToken)
-print "Found ", len(notebooks), " notebooks:"
-for notebook in notebooks:
-    print "  * ", notebook.name
+    @process_error
+    def connect(self):
+        if settings.get("authToken"):
+            return
+        def on_username(username):
+            def on_passwd(password):
+                if  username and  password:
+                    userStoreHttpClient = THttpClient.THttpClient(userStoreUri)
+                    userStoreProtocol = TBinaryProtocol.TBinaryProtocol(userStoreHttpClient)
+                    userStore = UserStore.Client(userStoreProtocol)
+                    authresult = userStore.authenticate(username,password,consumer_key,consumer_secret) 
+                    if authresult:
+                       token = authresult.authenticationToken
+                       noteStoreUrl = authresult.noteStoreUrl
+                       settings.set("authToken",token)  
+                       settings.set("noteStoreUrl",noteStoreUrl)  
+                    else:
+                        raise Exception("authenticate failure")
+            self.window.show_input_panel("password (required)::","",on_passwd,None,None) 
+        self.window.show_input_panel("username (required)::","",on_username,None,None)         
 
-# settings = sublime.load_settings("SublimeEvernote.sublime-settings")
+    def run(self, edit):
+        self.connect()
+        authToken = settings.get("authToken")
+        noteStoreUrl = settings.get('noteStoreUrl')
+        sublime.message_dialog(authToken+noteStoreUrl)  
 
-# class SendToEvernote(sublime_plugin.TextCommand):
-#     def __init__(self,view):
-#         self.view = view    
-#         self.token = None
+        noteStoreHttpClient = THttpClient.THttpClient(noteStoreUrl)
+        noteStoreProtocol = TBinaryProtocol.TBinaryProtocol(noteStoreHttpClient)
+        noteStore = NoteStore.Client(noteStoreProtocol)
 
-#     def run(self, edit):
-#         pass
+        # List all of the notebooks in the user's account        
+        notebooks = noteStore.listNotebooks(authToken)
+        print "Found ", len(notebooks), " notebooks:"
+        for notebook in notebooks:
+            print "  * ", notebook.name        
+
+
         
         
