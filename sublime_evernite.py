1209cd52bcb29907a81189532a29e46090339f15
jamiesun <jamiesun.net@gmail.com>
2012-07-15 14:34:30 +0800

    ...

--- sublime_evernite.py
+++ sublime_evernite.py
@@ -2,17 +2,12 @@
 import sys
 sys.path.append("lib")
 
-import hashlib
-import binascii
-import time
 from functools import wraps
 import thrift.protocol.TBinaryProtocol as TBinaryProtocol
 import thrift.transport.THttpClient as THttpClient
 import evernote.edam.userstore.UserStore as UserStore
-import evernote.edam.userstore.constants as UserStoreConstants
 import evernote.edam.notestore.NoteStore as NoteStore
 import evernote.edam.type.ttypes as Types
-import evernote.edam.error.ttypes as Errors
 
 import sublime,sublime_plugin
 
@@ -21,9 +16,6 @@ consumer_key = 'jamiesun-2467'
 consumer_secret ='7794453e92251986'
 evernoteHost = "www.evernote.com"
 userStoreUri = "https://" + evernoteHost + "/edam/user"
-
-
-
 
 settings = sublime.load_settings("SublimeEvernote.sublime-settings")
 
@@ -42,9 +34,7 @@ class SendToEvernoteCommand(sublime_plugin.TextCommand):
         self.window = view.window()
 
     @process_error
-    def connect(self):
-        if settings.get("authToken"):
-            return
+    def connect(self,callback):
         def on_username(username):
             def on_passwd(password):
                 if  username and  password:
@@ -56,27 +46,44 @@ class SendToEvernoteCommand(sublime_plugin.TextCommand):
                        token = authresult.authenticationToken
                        noteStoreUrl = authresult.noteStoreUrl
                        settings.set("authToken",token)  
-                       settings.set("noteStoreUrl",noteStoreUrl)  
+                       settings.set("noteStoreUrl",noteStoreUrl) 
+                       callback() 
                     else:
                         raise Exception("authenticate failure")
             self.window.show_input_panel("password (required)::","",on_passwd,None,None) 
-        self.window.show_input_panel("username (required)::","",on_username,None,None)         
+        self.window.show_input_panel("username (required)::","",on_username,None,None) 
 
-    def run(self, edit):
-        self.connect()
+    def send_note(self):
         authToken = settings.get("authToken")
         noteStoreUrl = settings.get('noteStoreUrl')
-        sublime.message_dialog(authToken+noteStoreUrl)  
-
         noteStoreHttpClient = THttpClient.THttpClient(noteStoreUrl)
         noteStoreProtocol = TBinaryProtocol.TBinaryProtocol(noteStoreHttpClient)
-        noteStore = NoteStore.Client(noteStoreProtocol)
+        noteStore = NoteStore.Client(noteStoreProtocol)        
+        region = sublime.Region(0L, self.view.size())
+        content = self.view.substr(region)        
+        def on_title():
+            note = Types.Note()
+            note.title = "Test note from EDAMTest.py"   
+            note.content = '<?xml version="1.0" encoding="UTF-8"?>'
+            note.content += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
+            note.content += '<en-note>%s'%content
+            note.content += '</en-note>'
 
-        # List all of the notebooks in the user's account        
-        notebooks = noteStore.listNotebooks(authToken)
-        print "Found ", len(notebooks), " notebooks:"
-        for notebook in notebooks:
-            print "  * ", notebook.name        
+            # Finally, send the new note to Evernote using the createNote method
+            # The new Note object that is returned will contain server-generated
+            # attributes such as the new note's unique GUID.
+            noteStore.createNote(authToken, note)      
+            sublime.message_dialog("send success")      
+
+        self.window.show_input_panel("username (required)::","",on_title,None,None) 
+
+    def run(self, edit):
+        if not settings.get("authToken"):
+            self.connect(self.send_note)
+        else:
+            self.send_note()
+ 
 
 
-    
+        
+        
