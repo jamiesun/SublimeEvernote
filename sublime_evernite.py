8369b2b2b525b21cc83722a3e36b8bc0f1caeb49
jamiesun <jamiesun.net@gmail.com>
2012-07-15 16:39:48 +0800

    ...

--- sublime_evernite.py
+++ sublime_evernite.py
@@ -1,7 +1,8 @@
 #coding:utf-8
 import sys
 sys.path.append("lib")
-
+import os
+import tempfile
 import thrift.protocol.TBinaryProtocol as TBinaryProtocol
 import thrift.transport.THttpClient as THttpClient
 import evernote.edam.userstore.UserStore as UserStore
@@ -15,7 +16,83 @@ consumer_secret ='7794453e92251986'
 evernoteHost = "www.evernote.com"
 userStoreUri = "https://" + evernoteHost + "/edam/user"
 
+LANGUAGES = {
+    'c': 'clike',
+    'cc': 'clike',
+    'cpp': 'clike',
+    'cs': 'clike',
+    'coffee': 'coffeescript',
+    'css': 'css',
+    'diff': 'diff',
+    'go': 'go',
+    'html': 'htmlmixed',
+    'htm': 'htmlmixed',
+    'js': 'javascript',
+    'json': 'javascript',
+    'less': 'less',
+    'lua': 'lua',
+    'md': 'markdown',
+    'markdown': 'markdown',
+    'pl': 'perl',
+    'php': 'php',
+    'py': 'python',
+    'pl': 'perl',
+    'rb': 'ruby',
+    'xml': 'xml',
+    'xsl': 'xml',
+    'xslt': 'xml'
+}
+
+DEPENDENCIES = {
+    'php': ['xml', 'javascript', 'css', 'clike'],
+    'markdown': ['xml'],
+    'htmlmixed': ['xml', 'javascript', 'css']
+}
+
 settings = sublime.load_settings("SublimeEvernote.sublime-settings")
+
+def to_html(contents,language,encoding='utf-8'):
+    contents = contents.replace('<', '&lt;').replace('>', '&gt;')
+    tmp_html = tempfile.NamedTemporaryFile(delete=False, suffix='.html')
+    tmp_html.write('<meta charset="%s">' % encoding)
+    # package manager path
+    plugin_dir = os.path.join(sublime.packages_path(), 'SublimeEvernote')
+    js = open(os.path.join(plugin_dir, 'codemirror', 'lib', 'codemirror.js'), 'r').read()
+    if language:
+        for dependency in DEPENDENCIES.get(language, []):
+            js += open(os.path.join(plugin_dir, 'codemirror', 'mode', dependency, '%s.js' % dependency), 'r').read()
+        js += open(os.path.join(plugin_dir, 'codemirror', 'mode', language, '%s.js' % language), 'r').read()
+    css = open(os.path.join(plugin_dir, 'codemirror', 'lib', 'codemirror.css'), 'r').read()
+
+    datas = {
+         'css': css,
+         'js': js,
+         'code': contents,
+         'mode': language
+    }
+    # theme
+    # <link rel="stylesheet" href="../theme/elegant.css">
+    html = u"""
+        <!doctype html>
+        <html>
+          <head>
+            <script>%(js)s</script>
+            <style>%(css)s</style>
+            <style>.CodeMirror-scroll {height: auto; overflow: visible;}</style>
+          </head>
+          <body>
+            <h3>%(title)s</h3>
+            <textarea id="code" name="code">%(code)s</textarea>
+            <script>
+            var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+                lineNumbers: true,
+                mode: "%(mode)s"
+            });
+            </script>
+          </body>
+        </html>
+    """ % datas
+    return html
 
 class SendToEvernoteCommand(sublime_plugin.TextCommand):
     def __init__(self,view):
@@ -53,7 +130,16 @@ class SendToEvernoteCommand(sublime_plugin.TextCommand):
             noteStoreProtocol = TBinaryProtocol.TBinaryProtocol(noteStoreHttpClient)
             noteStore = NoteStore.Client(noteStoreProtocol)        
             region = sublime.Region(0L, self.view.size())
-            content = self.view.substr(region)        
+            content = self.view.substr(region)    
+              
+            filename = self.view.file_name()
+            language = None
+            if filename:
+                fileext = os.path.splitext(filename)[1][1:]
+                language = LANGUAGES.get(fileext.lower())
+                
+            content = to_html(content,language)  
+
             def on_title(title):
                 note = Types.Note()
                 note.title = title
